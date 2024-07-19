f14c299c433cedbf25e80eb7c03d71bce1d38191
ffrrbb <franecomputer@gmail.com>
2024-07-19 16:51:54 +0200

    peer.rs

--- src/peer.rs
+++ src/peer.rs
@@ -70,7 +70,7 @@ pub(crate) struct PeerMap {
 
 impl PeerMap {
     pub(crate) async fn new() -> ResultType<Self> {
-        let db = std::env::var("DB_URL").unwrap_or_else(|| {
+        let db = std::env::var("DB_URL").unwrap_or({
             let mut db = "db_v2.sqlite3".to_owned();
             #[cfg(all(windows, not(debug_assertions)))]
             {
